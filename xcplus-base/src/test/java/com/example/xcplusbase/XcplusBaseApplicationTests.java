322671008fb312b08d0db193d937dc775e53b043
oliver <overl@local.com>
2024-02-01 17:47:36 +0800

    new parent pom.xml and base pom.xml

---
+++ xcplus-base/src/test/java/com/example/xcplusbase/XcplusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xcplusbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcplusBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
