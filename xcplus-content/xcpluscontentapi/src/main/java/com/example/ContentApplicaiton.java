ad76c955042bf726f0da8cb1fe0cb1b104c31bc6
oliver <overl@local.com>
2024-02-27 10:45:51 +0800

    0227，增加nacos配置和服务注册; 0227,增加网关，前端统一使用网关接入；

--- xcplus-content/xcpluscontentapi/src/main/java/com/example/ContentApplicaiton.java
+++ xcplus-content/xcpluscontentapi/src/main/java/com/example/ContentApplicaiton.java
@@ -4,6 +4,7 @@ package com.example;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2Doc
 @EnableSwagger2
 @SpringBootApplication
-
 @ComponentScan("com.example")
 public class ContentApplicaiton {
     public static void main(String[] args) {
